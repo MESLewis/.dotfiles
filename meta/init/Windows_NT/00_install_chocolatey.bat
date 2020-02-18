08b08ace0714b8b14879ed8e155082e078c4ee4e
Morgan Lewis <MESLewis@gmail.com>
2020-02-17 20:07:11 -0700

    Fix issue with chocolatey init script

--- meta/init/Windows_NT/00_install_chocolatey.bat
+++ meta/init/Windows_NT/00_install_chocolatey.bat
@@ -2,6 +2,6 @@ REM Install chocolatey if doesn't exist.
 WHERE /q choco
 IF ERRORLEVEL == 1 (
 echo "Installing chocolatey"
-@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command " [System.Net.ServicePointManager]REMSecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command " [System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 refreshenv
 )
